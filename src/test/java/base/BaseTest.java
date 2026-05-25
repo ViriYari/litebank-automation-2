e545515bf0adafe21c7b69ebbcac417d453d2d7a
ViriYari <57279430+ViriYari@users.noreply.github.com>
2026-05-25 16:18:38 -0600

    fix: udos

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,6 @@
 package base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
